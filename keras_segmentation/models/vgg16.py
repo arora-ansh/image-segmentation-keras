2ae496175d596dde657a9b0d60153b5dc3a56103
Ansh Arora <ansh19022@iiitd.ac.in>
2022-04-29 14:48:43 +0530

    Update vgg16.py

--- keras_segmentation/models/vgg16.py
+++ keras_segmentation/models/vgg16.py
@@ -74,7 +74,7 @@ def get_vgg_encoder(input_height=224,  input_width=224, pretrained='imagenet', c
     f5 = x
 
     if pretrained == 'imagenet':
-        VGG_Weights_path = keras.utils.get_file(
+        VGG_Weights_path = keras.utils.all_utils.get_file(
             pretrained_url.split("/")[-1], pretrained_url)
         Model(img_input, x).load_weights(VGG_Weights_path, by_name=True, skip_mismatch=True)
 
